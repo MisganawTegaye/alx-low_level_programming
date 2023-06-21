213474d7e13c6431c6554ff67a200f53ca17d038
MisganawTegaye <misganatsegaye23@gmail.com>
2023-06-20 23:49:25 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,19 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
 		return (0);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
-	}
 		return (-1);
+	}
+
+	/* Add a return statement if n does not fall into any of the conditions above */
+	return (0);
 }
 
