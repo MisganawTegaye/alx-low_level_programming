0820e2f43475deee0dce54e12108bcc62f5f0f17
MisganawTegaye <misganatsegaye23@gmail.com>
2023-06-29 13:27:56 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    int len1 = strlen(n1); // length of n1
+    int len2 = strlen(n2); // length of n2
+
+    int carry = 0; // carry value for addition
+    int i = len1 - 1; // index for n1
+    int j = len2 - 1; // index for n2
+    int k = size_r - 1; // index for result string (r)
+
+    // Ensure there is enough space in r to store the result
+    if (len1 + len2 >= size_r)
+        return 0;
+
+    // Perform addition digit by digit from right to left
+    while (i >= 0 || j >= 0 || carry != 0)
+    {
+        int sum = carry;
+
+        if (i >= 0)
+            sum += n1[i--] - '0';
+
+        if (j >= 0)
+            sum += n2[j--] - '0';
+
+        carry = sum / 10;
+        sum %= 10;
+
+        r[k--] = sum + '0';
+    }
+
+    // Move the result to the beginning of the string
+    memmove(r, r + k + 1, size_r - k - 1);
+
+    return r;
+}
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+    char n1[] = "123456789";
+    char n2[] = "987654321";
+    char result[20];
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+    char *res = infinite_add(n1, n2, result, sizeof(result));
+
+    if (res != 0)
+        printf("Sum: %s\n", res);
+    else
+        printf("Result could not be stored in the buffer.\n");
+
+    return 0;
 }
 
