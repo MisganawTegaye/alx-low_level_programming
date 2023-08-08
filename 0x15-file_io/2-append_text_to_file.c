f06cbde9f4651c38cdbad2f81b25fce054a66cda
MisganawTegaye <misganatsegaye23@gmail.com>
2023-08-08 06:27:33 -0700

    0x15-file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,11 +7,11 @@
  *
  * Return: If the function fails or filename is NULL - -1.
  *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
+ *     - 1 Otherwise.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, b, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	a = open(filename, a_WRONLY | a_APPEND);
-	b = write(a, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (a == -1 || w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(a);
+	close(o);
 
 	return (1);
 }
